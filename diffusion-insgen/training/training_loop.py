b4c1f5ed55cd10993d8454ea0ea2f25ec1299fbc
Zhendong <zw2533@columbia.com>
2022-10-17 11:02:22 -0500

    update diffusion-insgen training_loop.py

--- diffusion-insgen/training/training_loop.py
+++ diffusion-insgen/training/training_loop.py
@@ -424,7 +424,7 @@ def training_loop(
         snapshot_data = None
         if (network_snapshot_ticks is not None) and (done or cur_tick % network_snapshot_ticks == 0):
             snapshot_data = dict(training_set_kwargs=dict(training_set_kwargs))
-            for name, module in [('G', G), ('D', D), ('G_ema', G_ema), ('augment_pipe', augment_pipe)]:
+            for name, module in [('G', G), ('D', D), ('G_ema', G_ema), ('augment_pipe', augment_pipe), ('D_ema', D_ema), ('DHead', DHead), ('GHead', GHead)]:
                 if module is not None:
                     if num_gpus > 1:
                         misc.check_ddp_consistency(module, ignore_regex=r'.*\.w_avg')
